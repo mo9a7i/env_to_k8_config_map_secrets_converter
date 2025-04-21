c5bcae3bdb2aa959a9a7a3e82f744a9e45feaa0a
Mohannad Otaibi <mohannad.otaibi@gmail.com>
2025-04-21 18:59:21 +0300

    deploy file

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,9 @@ import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://mo9a7i.github.io',
+  base: 'env_to_k8_config_map_secrets_converter',
+
   vite: {
       plugins: [tailwindcss()]
     },
